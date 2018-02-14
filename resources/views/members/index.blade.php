4d6e263ad87b6e7ae0dddd952d276bbad530d9d7
unknown <gabrielizuniga@students.abtech.edu>
2018-02-13 19:57:39 -0500

    Getting ready to build orders and members views

---
+++ resources/views/members/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+      <meta charset="utf-8">
+      <title>Index</title>
+  </head>
+
+  <body>
+      <h1>Members</h1>
+  </body>
+
+</html>
---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+      <meta charset="utf-8">
+      <title>Index</title>
+  </head>
+
+  <body>
+      <h1>Orders</h1>
+  </body>
+
+</html>
