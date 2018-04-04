5ac26fa1e95af094cd405647775f23b43d2d3208
unknown <gabrielizuniga@students.abtech.edu>
2018-04-03 20:17:04 -0400

    Part 3 completed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,11 +14,44 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         DB::table('coffees')->insert([
             'name'  => 'Suplicar Clemencia Dark',
-            'roast' => 'dark',
-            'desc'   => 'We take a specially selected blend of beans and roast them until they beg for mercy. A roasty, chocolaty, full bodied and honey-sweet dark roast.',
+            'roast' => 'dark', 
+            'desc'   => 'We take a specially selected blend of beans and roast them until they beg for mercy. A roasty, chocolaty, full bodied and honey-sweet dark roast.', 
             'country' => 'Not available',
-            'image' => str_random(10)
+            'image' => str_random(10), 
         ]);
 
+       	DB::table('coffees')->insert([
+            'name'  => 'Mexico Chiapas Altura',
+            'roast' => 'medium', 
+            'desc'   => 'This beauty is comfortingly sweet, boasting a medium body with milk chocolate flavors interlaced with light spice and a sweet, mild finish. A really great way to start your morning! Certified Organic and Fair Trade.', 
+            'country' => 'Mexico',
+            'image' => str_random(10), 
+        ]);
+
+        DB::table('coffees')->insert([
+            'name'  => 'Ethiopia Sidamo',
+            'roast' => 'medium', 
+            'desc'   => 'We love the shear complexity of this traditional natural-processed beauty. Lots of lemongrass, slightly floral and a backnote of berries in the finish.  From the small-scale farmers of the Sidama Coffee Farmers Cooperative Union. Certified Organic and Fair Trade.', 
+            'country' => 'Ethiopia',
+            'image' => str_random(10), 
+        ]);
+
+        DB::table('users')->insert([
+            'name'  => 'Søren Kierkegaard',
+            'email' => 'soren@coffe.com',
+            'password' => str_random(32),
+        ]);
+
+        DB::table('users')->insert([
+            'name'  => 'Honore de Balzac',
+            'email' => 'honore@coffe.com',
+            'password' => str_random(32),
+        ]);
+
+        DB::table('users')->insert([
+            'name'  => 'Theodore Roosevelt',
+            'email' => 'theodore@coffe.com',
+            'password' => str_random(32),
+        ]);
     }
 }
